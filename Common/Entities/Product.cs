4a0111d802ad654bc2e8662cbd10fa7cede711b5
Eziz Rehimov <fantastig@bk.ru>
2023-12-06 17:26:30 +0400

    Api 1 , basic create with Ntier

---
+++ Common/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using Common.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Entities
+{
+    public class Product:BaseEntity
+    {
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public int Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
